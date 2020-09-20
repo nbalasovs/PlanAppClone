e0121df3c266dda068dabde6a692201f274cd173
Nikaels Balasovs <nikaelsbalasovs@MacBook-Pro.local>
2020-09-20 17:20:03 +0200

    Revert "production updates"
    This reverts commit 50f426d7c5ceb83725fadaba73a7d4e4291ed2e4.

--- server/index.js
+++ server/index.js
@@ -21,13 +21,6 @@ app.use('/api', require('./routes/register'))
 app.use('/api', require('./routes/auth'))
 app.use('/api/course', require('./routes/courses'))
 
-if (!process.env.NODE_ENV || process.env.NODE_ENV.toString().trim() !== 'production') {
-  app.use(express.static('client/dist'))
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
-  })
-}
-
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useCreateIndex: true,
